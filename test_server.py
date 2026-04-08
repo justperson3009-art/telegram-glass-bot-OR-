07344225b825125517fce30a10182dfff43bdb18
User <user@example.com>
2026-04-08 23:50:05 +0300

    chore: обновлён токен + удалён test_server.py

--- test_server.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import ssl
-import json
-
-ctx = ssl.create_default_context()
-url = 'https://149.154.166.110/botREDACTED_TOKEN/getMe'
-req = urllib.request.Request(url, headers={'Host': 'api.telegram.org'})
-r = urllib.request.urlopen(req, context=ctx, timeout=10)
-print(r.read().decode())
